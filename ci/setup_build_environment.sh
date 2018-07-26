02751b67566e4951b5e2fe25acb3b4ed5a56684d
Ryan Burn <ryan.burn@gmail.com>
2018-07-25 20:04:38 -0700

    Set up testing.

---
+++ ci/setup_build_environment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+apt-get update 
+apt-get install --no-install-recommends --no-install-suggests -y \
+                ca-certificates \
+                build-essential \
+                software-properties-common \
+                cmake \
+                pkg-config \
+                git \
+                automake \
+                autogen \
+                autoconf \
+                libtool \
+                ssh \
+                wget \
+                curl \
+                unzip \
+                libreadline6-dev \
+                libncurses5-dev
